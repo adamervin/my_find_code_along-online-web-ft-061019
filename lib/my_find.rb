82b431d59d5cb677b5ed4b49b7f350d71f80be8e
Adam Ervin <adamjervin@gmail.com>
2019-06-21 20:35:36 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,8 +2,8 @@
 def my_find(collection)
 i = 0 
 while i < collection.length
-  return collection[i]
-if yield(collection[i])
+  return collection[i] if 
+  yield(collection[i])
   i = i + 1 
  end
 end
